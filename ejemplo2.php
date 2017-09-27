ba9715dca7bc84eb3b7d439286576c2bac99023c
wilsongus2003 <wilsogus2003@yahoo.com>
2017-09-27 17:32:46 -0500

    ingrese mensahe github

---
+++ ejemplo2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>programa</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<?php
+	echo "saludos...para git hub";
+?>
+</body>
+</html>
